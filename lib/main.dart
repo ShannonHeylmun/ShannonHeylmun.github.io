c916dd51bca52e36935982e942ec135c8f03de5e
Shannon Heylmun <sajshannon@gmail.com>
2026-03-12 02:03:58 -0400

    EnsureSemantics for web accessibility

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // import 'package:flutter/services.dart';
 import 'package:sh_portfolio/my_app.dart';
+import 'package:flutter/semantics.dart';
 
 import 'package:logging/logging.dart';
 
 final log = Logger('Logger');
-
+const bool kIsWeb = bool.fromEnvironment('dart.library.js_interop');
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Logger.root.level = Level.ALL; // defaults to Level.INFO
@@ -18,4 +19,7 @@ void main() {
     DeviceOrientation.portraitDown,
   ]);
   runApp(const MyApp());
+  if (kIsWeb) {
+    SemanticsBinding.instance.ensureSemantics();
+  }
 }
